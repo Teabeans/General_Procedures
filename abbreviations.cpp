085838dfc83059bff0789cc3b7da91a6e0273f55
Tim Lum <twhlum@gmail.com>
2018-06-27 12:50:36 -0700

    Create abbreviations.cpp

---
+++ abbreviations.cpp
@@ -0,0 +1,103 @@
+//-------|---------|---------|---------|---------|---------|---------|---------|
+//
+// ABBREVIATIONS - General Reference
+// abbreviations.cpp
+//
+//-------|---------|---------|---------|---------|---------|---------|---------|
+
+//-----------------------------------------------------------------------------|
+// Authorship
+//-----------------------------------------------------------------------------|
+//
+// Tim Lum
+// twhlum@gmail.com
+// Created:  2018.06.27
+// Modified: 2018.06.27
+//
+
+
+//-----------------------------------------------------------------------------|
+// Abbreviations
+//-----------------------------------------------------------------------------|
+//
+// (-)      == (UML) Private
+// (#)      == (UML) Protected
+// (+)      == (UML) Public
+// !        == Not
+// @        == At
+// #b2c     == #-Bit 2's Complement
+// Arg      == Argument
+// Alloc    == Allocate(d)
+// B-1st    == Breadth-First
+// BH       == Binary Heap
+// BST      == Binary Search Tree
+// BT       == Binary Tree
+// Char     == Character
+// Coeff    == Coefficient
+// Concat   == Concatenation
+// Curr     == Current
+// D-1st    == Depth-First
+// DB       == Database
+// Dbl      == Double
+// Desc     == Description
+// Dest     == Destination
+// deQ      == Dequeue
+// Dist     == Distance
+// enQ      == Enqueue
+// EoF      == End of File
+// EoL      == End of Line
+// EQ       == Equals
+// Expon    == Exponent
+// Exts     == Extends
+// FIFO     == First In, First Out (Queue)
+// Gen      == Generic
+// Info     == Information
+// Inhrt    == Inherit
+// Int      == Integer
+// Invar    == Invariant
+// L        == Left
+// LIFO     == Last In, First Out (Stack)
+// LH       == Left-Hand
+// LL       == Linked List
+// LSB      == Least Significant Bit
+// LSD      == Least Significant Digit
+// Malloc   == Memory Allocate(d)
+// MetCall  == Method Call
+// Mo3      == Median of Three
+// MSB      == Most Significant Bit
+// MSD      == Most Significant Digit
+// NaN      == Not a Number
+// NaV      == Not a Value
+// Obj      == Object
+// Param    == Parameter
+// Polynom  == Polynomial
+// PosCond  == Postcondition
+// PreCond  == Precondition
+// Prev     == Previous
+// Ptr      == Pointer
+// Q        == Queue
+// R        == Right
+// Ret      == Return
+// RetVal   == Return Value
+// RGB      == Red Green Blue
+// (R)GB    == Red
+// R(G)B    == Green
+// RG(B)    == Blue
+// RH       == Right-Hand
+// RnTm     == Runtime
+// RtL      == Right to Left
+// SOP      == Shortest Open Path
+// Src      == Source
+// Str      == String
+// <T>      == "Of type T"
+// Term     == Terminus
+// Trav     == Traverse / Traversal
+ // PreOrd  == Pre-Order
+ // InOrd   == In-Order
+ // PostOrd == Post-Order
+// TtB      == Top to Bottom
+// Ur-      == Originator, foremost (ie. Ur-Node)
+// Val      == Value
+// Var      == Variable
+// Vtx      == Vertex
+//
